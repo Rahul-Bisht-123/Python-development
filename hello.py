f4091a404a49aebd4d95d2793bfb0280804ea511
Rahul-Bisht-123 <rbrahulbisht2000@gmail.com>
2023-08-20 17:50:42 +0530

    File 1

---
+++ hello.py
@@ -0,0 +1,10 @@
+# exploring the print() function of python
+
+print('this line will get print')
+print(55+33)
+print('ram'+'singh')
+print(True)
+print(False)
+print(7.99+6.55)
+print(1_00_000)
+print(5.9 + 99)
