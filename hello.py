bab949ad5000c5971ee5a6a083426f94eab31dc5
Rahul-Bisht-123 <rbrahulbisht2000@gmail.com>
2023-08-27 09:54:41 +0530

    file5 || Unit6

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
 # exploring the print() function of python
 
-print('this line will get print')
-print(55+33)
-print('ram'+'singh')
-print(True)
-print(False)
-print(7.99+6.55)
-print(1_00_000)
-print(5.9 + 99)
+# print('this line will get print')
+# print(55+33)
+# print('ram'+'singh')
+# print(True)
+# print(False)
+# print(7.99+6.55)
+# print(1_00_000)
+# print(5.9 + 99)
+
+# ----------------helper function for module.py----------
+
+def fib(n):
+    fibo = []
+    a,b = 0,1
+    while a < n:
+        fibo.append(a)
+        a,b = b,a+b
+    return fibo
+
---
+++ modules.py
@@ -0,0 +1,24 @@
+#----------------------------------------------------------|
+#                   Unit - 6  | File 5                     |
+#                     " Modules "                          |
+#     (This is file 5 containing notes of pydocs unit-6)   |
+#----------------------------------------------------------|
+
+
+# function that returns fibonacci series upto n
+# import hello (we can write this also)
+from hello import fib
+
+fib1 = fib(10)
+print(fib1)
+# [0, 1, 1, 2, 3, 5, 8]
+'thus we succesfully imported file hello'
+"NOTE:"
+'''
+We can use many ways to import any module.for ex:
+
+import hello
+from hello import fib
+from hello import * (star means everything)
+from hello import fib as fb
+'''    
